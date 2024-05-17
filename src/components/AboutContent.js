e9371833aae9a31f019bae9bea813865e863adfe
Dhanasriganesh <dhanasriganesh9@gmail.com>
2024-05-17 21:38:38 +0530

    second

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -8,7 +8,7 @@ const AboutContent = () => {
   return (
     <div className="about">
       <div className="left">
-        <h1>Who Am I?</h1>
+        <h1>Who I am?</h1>
         <p>I'm a react front-end developer.I create Responsive secure websites for my clients.</p>
         <Link to="/contact">
             <button className="btn">Contact</button>
